f5fe35eeff2a24734b84371aa3342a1cafe282c9
Jay Magar <creatorjay41@gmail.com>
2026-06-17 00:22:44 +0530

    Fix Vercel build errors by upgrading Next and React to stable

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+import path from "path";
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  experimental: {
+    outputFileTracingRoot: path.join(process.cwd(), "../../"),
+  },
 };
 
 export default nextConfig;
